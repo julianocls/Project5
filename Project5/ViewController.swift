196801588704f27e40362e7c502ac9d5f15539f7
Juliano C L Santos <foxports@hotmail.com>
2022-06-18 01:45:40 -0300

    Preparando  para o envio: letras minúsculas() e IndexPath

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     var allWords = [String]()
-    var userWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,34 +33,57 @@ class ViewController: UITableViewController {
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
-        
-        let submetAction = UIAlertAction(title: "Submit", style: .default) { [ weak self, weak ac] action in
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
             guard let answer = ac?.textFields?[0].text else { return }
             self?.submit(answer)
         }
-        
-        ac.addAction(submetAction)
+
+        ac.addAction(submitAction)
         present(ac, animated: true)
     }
     
-    func submit(_ answer: String) {
-        print(answer)
-    }
-    
-    func startGame() {
-        title = allWords.randomElement()
-        userWords.removeAll(keepingCapacity: true)
-        tableView.reloadData() //chama numberOfRowsInSection novamente
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return userWords.count
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-        cell.textLabel?.text = userWords[indexPath.row]
+        cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData() //chama numberOfRowsInSection novamente
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        return true
     }
 }
 
