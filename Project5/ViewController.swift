db081f47e23014101a280010815187e917862ddd
Juliano C L Santos <foxports@hotmail.com>
2022-06-21 00:51:32 -0300

    Desafio impedir palavras com menos de 3 dígitos corrigir bug impedir digitar palavra inicial

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -15,7 +15,11 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        let btnAdd = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        let btnRestart = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(restartGame))
+
+        toolbarItems = [btnAdd, btnRestart]
+        navigationController?.isToolbarHidden = false
 
         if let startWorkdsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWorkdsURL) {
@@ -27,6 +31,10 @@ class ViewController: UITableViewController {
             allWords = ["silkworm"]
         }
         
+        startGame()
+    }
+    
+    @objc func restartGame() {
         startGame()
     }
     
@@ -56,32 +64,28 @@ class ViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
 
-        let errorTitle: String
-        let errorMessage: String
-
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(lowerAnswer, at: 0)
 
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
 
                     return
                 } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage("Word not recognised", "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
+                showErrorMessage("Word used already", "Be more original!")
             }
         } else {
             guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage("Word not possible", "You can't spell that word from \(title)")
         }
-
+    }
+    
+    func showErrorMessage(_ errorTitle: String, _ errorMessage: String) {
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
@@ -112,6 +116,10 @@ class ViewController: UITableViewController {
     }
     
     func isReal(word: String) -> Bool {
+        if title == word || word.utf16.count < 3 {
+            return false
+        }
+        
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "pt_BR")
